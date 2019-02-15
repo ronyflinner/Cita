d8f999848b21812b7e2218c91f567cc8fcba41f7
Luis Gomez <luiskaco@gmail.com>
2019-02-15 14:09:47 -0500

    Ultimos Cambios

--- database/migrations/2019_01_09_162235_create_citas_table.php
+++ database/migrations/2019_01_09_162235_create_citas_table.php
@@ -18,8 +18,24 @@ class CreateCitasTable extends Migration {
 
 			$table->integer('disponibilidad_id')->unsigned();
 			$table->foreign('disponibilidad_id')->references('id')->on('disponibilidads')->onDelete('cascade');
-
+			$table->integer('status');
 			$table->integer('status_asistio');
+			$table->integer('status_pago');
+			/*Codigo de Referencia*/
+			$table->string('referenceCode')->nullable(true);
+			$table->string('processingDate')->nullable(true);
+			$table->string('buyerEmail')->nullable(true);
+			$table->string('transactionId')->nullable(true);
+			$table->string('reference_pol')->nullable(true);
+
+			/*Tipo de pago*/
+			$table->string('lapPaymentMethod')->nullable(true);
+			$table->string('lapPaymentMethodTyp')->nullable(true);
+
+			/*Estados de la transferecnia*/
+			$table->string('message')->nullable(true);
+			$table->string('transactionState')->nullable(true);
+			$table->string('lapTransactionState')->nullable(true);
 
 			$table->string('slug')->unique();
 			$table->timestamps();
