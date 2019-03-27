9c8233fce5b764e8c298a14fd9f03e434993e149
Ronald Lindo <ronald94af@gmail.com>
2019-03-27 13:28:08 -0500

    cambio2

--- resources/views/admin/programarCita.blade.php
+++ resources/views/admin/programarCita.blade.php
@@ -217,14 +217,14 @@
 
 </div>
 
-<div class="links">
-    <form method="post" action="{{url('admin/import-excel')}}" enctype="multipart/form-data">
-        {{csrf_field()}}
+<!-- <div class="links">
+    <form method="post" action="{ {url('admin/import-excel')}}" enctype="multipart/form-data">
+        { {csrf_field()}}
         <input type="file" name="excel">
         <br><br>
         <input type="submit" value="Enviar" style="padding: 10px 20px;">
     </form>
-</div>
+</div> -->
 @endsection
 
 @section('javascript')
